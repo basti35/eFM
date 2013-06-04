39bfe3eb0ae3e3832cb1b04f176dd21b352ff2c9
basti35 <basti35@gmail.com>
2013-06-04 03:13:31 -0700

    Modification to provider model, setting for null and blank

--- provider/models.py
+++ provider/models.py
@@ -4,19 +4,19 @@ from django.db import models
 
 #provider profile
 class Serprov(models.Model):
-  yp_page = models.URLField(max_length=150)
+  yp_page = models.URLField(max_length=150, null=True, blank=True)
   name = models.CharField(max_length=150)
-  owner = models.CharField(max_length=150)
-  logo = models.CharField(max_length=80)
+  owner = models.CharField(max_length=150, null=True, blank=True)
+  logo = models.CharField(max_length=80, null=True, blank=True)
   address = models.CharField(max_length=80)
   full_city = models.CharField(max_length=399)
   city = models.CharField(max_length=29)
-  tel = models.CharField(max_length=40)
-  fax = models.CharField(max_length=50)
-  mail = models.EmailField(max_length=254)
-  description = models.CharField(max_length=1500)
-  url = models.URLField(max_length=80)
-  others = models.CharField(max_length=100)
+  tel = models.CharField(max_length=40, null=True, blank=True)
+  fax = models.CharField(max_length=50, null=True, blank=True)
+  mail = models.EmailField(max_length=254, null=True, blank=True)
+  description = models.CharField(max_length=1500, null=True, blank=True)
+  url = models.URLField(max_length=80, null=True, blank=True)
+  others = models.CharField(max_length=100, null=True, blank=True)
 
   BUILDING_CONSTRUCTIONS=1
   ELECTRICIAN=2
@@ -47,14 +47,14 @@ class Serprov(models.Model):
     (WHITEWASHING, 'whitewashing'),
     (CLEANING_SERVICES, 'cleaning services'))
 
-  field = models.IntegerField(choices=FIELD_CHOICES)
+  field = models.IntegerField(choices=FIELD_CHOICES, null=True)
   hourly_price= models.DecimalField(max_digits=5, decimal_places=2, blank=False)
-  longitude = models.FloatField()
-  latitude = models.FloatField()
+  longitude = models.FloatField(blank=True)
+  latitude = models.FloatField(blank=True)
   avg_rate = models.DecimalField(max_digits=2, decimal_places=1, null=True)
   n_rates = models.IntegerField(null=True)
-  password= models.CharField(max_length=30)
-  pass_md5= models.CharField(max_length=50)
+  password= models.CharField(max_length=30, blank=False)
+  pass_md5= models.CharField(max_length=50, blank=True)
   def __unicode__(self):
     return self.name
 
@@ -91,7 +91,7 @@ class Intervention(models.Model):
     (CONDITIONING_FAILURE, 'conditioning failure'))
   failure = models.IntegerField(choices=FAILURE_TYPES)
   
-  serprov = models.ForeignKey(Serprov, blank=True, null=True)
+  serprov = models.ForeignKey(Serprov, blank=False, null=True)
   firstAvailability=models.DateTimeField()
   def __unicode__(self):
     return self.name
@@ -102,7 +102,7 @@ class Intervention(models.Model):
 class Feedback_serprov(models.Model):
   intervention = models.OneToOneField(Intervention, blank=True, null=True)
   serprov = models.ForeignKey(Serprov, blank=True, null=True)
-  rate = models.DecimalField(max_digits=2, decimal_places=1)
+  rate = models.DecimalField(max_digits=2, decimal_places=1, blank=False)
   note = models.CharField(max_length=250)
   def __unicode__(self):
     return self.name
